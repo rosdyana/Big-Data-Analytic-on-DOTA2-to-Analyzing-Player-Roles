ee0f3100d835d896cce8290632c076626330b9f9
Rosdyana Kusuma <rosdyana.kusuma@gmail.com>
2017-06-17 11:50:47 +0800

    - newest method to preprocess the data

--- codes/draft_code.R
+++ codes/draft_code.R
@@ -5,16 +5,6 @@ library(ROpenDota)
 # write.table(heroes[,1:5], file = "heroes.csv",row.names=FALSE, na="", sep=",")
 # loadHeroes <- data.frame(read.csv("heroes.csv", header = T, sep = ","))
 
-# hero roles
-# carry
-carry = c(1,4,6,8,9,10,11,12,13,15,17,18,19,21,23,28,32,34,35,36,39,41,42,43,44,46,47,48,49,52,53,54,56,59,60,61,63,67,69,70,71,72,73,74,75,76,77,78,80,81,82,89,93,94,95,99,104,106,109,113,114)
-support = c(3,5,7,20,25,26,27,30,31,37,40,50,57,58,64,66,68,79,83,84,85,86,87,90,91,92,101,102,108,110,111,112)
-initiator = c(2,7,13,16,29,38,51,55,65,97,100,103,96)
-nuker = c(22,45,98,105,107)
-escape = c(62)
-disabler = c(88,14,33)
-# total = length(carry)+length(support)+length(initiator)+length(nuker)+length(escape)+length(disabler)
-
 # get all items and save it to csv
 # items = data.frame(get_items())
 # write.table(items[,1:6], file = "items.csv",row.names=FALSE, na="", sep=",")
@@ -30,88 +20,245 @@ disabler = c(88,14,33)
 # BOSTON = read.csv("BOSTONMAJOR.csv", header = T, sep = ",")
 # merge the data
 # matchesData <- rbind(MANILA, SHANGHAI, BOSTON)
- matchesData <- read.csv("TI2016.csv", header = T, sep = ",")
-
+matchesData <- read.csv("EPICENTER2.csv", header = T, sep = ",")
+# matchesData <- rbind(TI2016)
 # the data consist 10 match id in one match because 10 player have own macth id
 matchesId <- unique(matchesData$match_id)
-# x = get_match_details(3188810567)
-# x$players$
-# dataset format 
-# accountid ,level, kda, kill, death, assist , neutral kill, obs, sen, GPM, XPM, last hit, denied, Hero damaged, hero healing, tower damage,tower kill, gold, XP, hero as class
-for ( i in matchesId )
+# x = get_match_details(3240966096)
+# aa = data.frame(x$players$purchase)
+# if(!is.na(aa)) print("AA") else print("BB")
+
+# dataset format
+# accountid ,level, kda, kill, death, assist , neutral kill, GPM, XPM, last hit, denied, Hero damaged, hero healing, tower damage,tower kill, gold, XP, hero as class
+for (z in matchesId)
 {
-  print(i)
-  x = get_match_details(i)
-  account_id = x$player$account_id
+  print(z)
+  x = get_match_details(z)
+  
   level = x$player$level
   kda = x$players$kda
   kill = x$players$kills
-  if(is.null(kill)) kill=0
+  if (is.null(kill))
+    kill = 0
   death = x$players$deaths
-  if(is.null(death)) death=0
+  if (is.null(death))
+    death = 0
   assist = x$players$assists
-  if(is.null(assist)) assist=0
+  if (is.null(assist))
+    assist = 0
   NK = x$players$neutral_kills
-  if(is.null(NK)) NK=0
-  obs = x$players$obs_placed
-  if(is.null(obs))obs=0
-  sen = x$players$sen_placed
-  if(is.null(sen)) sen=0
+  if (is.null(NK))
+    NK = 0
   GPM = x$players$gold_per_min
-  if(is.null(GPM)) GPM=0
+  if (is.null(GPM))
+    GPM = 0
   XPM = x$players$xp_per_min
-  if(is.null(XPM)) XPM=0
+  if (is.null(XPM))
+    XPM = 0
   LH = x$players$last_hits
-  if(is.null(LH)) LH=0
+  if (is.null(LH))
+    LH = 0
   DN = x$players$denies
-  if(is.null(DN)) DN=0
+  if (is.null(DN))
+    DN = 0
   HD = x$players$hero_damage
-  if(is.null(HD)) HD=0
+  if (is.null(HD))
+    HD = 0
   HH = x$players$hero_healing
-  if(is.null(HH)) HH=0
+  if (is.null(HH))
+    HH = 0
   TD = x$players$tower_damage
-  if(is.null(TD)) TD=0
+  if (is.null(TD))
+    TD = 0
   TK = x$players$tower_kills
-  if(is.null(TK)) TK=0
+  if (is.null(TK))
+    TK = 0
   gold = x$players$total_gold
-  if(is.null(gold)) gold=0
+  if (is.null(gold))
+    gold = 0
   XP = x$players$total_xp
-  if(is.null(kill)) kill=0
+  if (is.null(kill))
+    kill = 0
   hero = x$players$hero_id
+  eff = x$players$lane_efficiency
+  if (is.null(eff))
+    eff = 0
+  lane = x$players$lane_role
+  if (is.null(lane))
+    lane = 0
+  checkMe = data.frame(x$players$purchase)
+  Observer = 0
+  Sentry = 0
+  if (!is.na(checkMe)) {
+    Observer = x$players$purchase$ward_observer
+    Observer[is.na(Observer)] = 0
+    Sentry = x$players$purchase$ward_sentry
+    Sentry[is.na(Sentry)] = 0
+  }
   
-  y = data.frame(account_id, level, kda, kill, death, assist,obs, sen, NK, GPM, XPM, LH, DN, HD, HH, TD,TK, gold, XP, hero )
+  # class
+  players = length(x$players$account_id)
+  isRadiant = x$players$isRadiant
+  # radiant
+  radEFF = x$player$lane_efficiency_pct[1:5]
+  if (is.null(radEFF))
+    radEFF = 0
+  radEFF[is.na(radEFF)] <- 0
+  
+  radroam = x$players$is_roaming[1:5]
+  if (is.null(radroam))
+    radEFF = FALSE
+  radroam[is.na(radroam)] <- FALSE
+  
+  radlane = x$players$lane[1:5]
+  
+  radgold = x$players$gold[1:5]
+  if (is.null(radgold))
+    radgold = 0
+  radgold[is.na(radgold)] <- 0
+  
+  radObserver = 0
+  if (!is.na(checkMe)) {
+    radObserver = x$players$purchase$ward_observer[1:5]
+    radObserver[is.na(radObserver)] = 0
+  }
+  
+  radSentry = 0
+  if (!is.na(checkMe)) {
+    radSentry = x$players$purchase$ward_sentry[1:5]
+    radSentry[is.na(radSentry)] = 0
+  }
+  
+  # # dire
+  dirEFF = x$player$lane_efficiency_pct[6:10]
+  if (is.null(dirEFF))
+    dirEFF = 0
+  dirEFF[is.na(dirEFF)] <- 0
+  
+  dirroam = x$players$is_roaming[6:10]
+  if (is.null(dirroam))
+    radEFF = FALSE
+  dirroam[is.na(dirroam)] <- FALSE
+  
+  dirObserver = 0
+  if (!is.na(checkMe)) {
+    dirObserver = x$players$purchase$ward_observer[6:10]
+    dirObserver[is.na(dirObserver)] = 0
+  }
+  
+  dirSentry = 0
+  if (!is.na(checkMe)) {
+    dirSentry = x$players$purchase$ward_sentry[6:10]
+    dirSentry[is.na(dirSentry)] = 0
+  }
+  
+  dirlane = x$players$lane[6:10]
+  
+  dirgold = x$players$gold[6:10]
+  if (is.null(dirgold))
+    dirgold = 0
+  dirgold[is.na(dirgold)] <- 0
+  m_class = c()
+  for (i in 1:players) {
+    if (isRadiant[i])
+    {
+      if (radgold[i] == max(radgold)) {
+        rolesx = "carry"
+      } else if (radEFF[i] == radEFF[order(radEFF)[4]]) {
+        rolesx = "carry"
+      } else if (radEFF[i] == radEFF[order(radEFF)[5]]) {
+        rolesx = "midlaner"
+      } else if (radlane[i] == 2) {
+        rolesx = "midlaner"
+      } else if (radEFF[i] == radEFF[order(radEFF)[3]]) {
+        rolesx = "offlaner"
+      } else if (radgold[i] == radgold[order(radgold)[3]]) {
+        rolesx = "offlaner"
+      } else if (radroam[i]) {
+        rolesx = "roaming_support"
+      } else if (radSentry[i] == max(radSentry)) {
+        rolesx = "hard_support"
+      } else if (radObserver[i] == max(radObserver)) {
+        rolesx = "hard_support"
+      } else if (radlane[i] == 3) {
+        rolesx = "offlaner"
+      } else if (radObserver[i] != 0 && radObserver[i] < max(radObserver)) {
+        rolesx = "roaming_support"
+      } else if (radSentry[i] != 0 && radSentry[i] < max(radSentry)) {
+        rolesx = "roaming_support"
+      } else
+        rolesx = "unknown"
+      m_class <- c(m_class, rolesx)
+    } else {
+      if (dirgold[i - 5] == max(dirgold)) {
+        rolesx = "carry"
+      } else if (dirEFF[i - 5] == dirEFF[order(dirEFF)[4]]) {
+        rolesx = "carry"
+      } else if (dirEFF[i - 5] == dirEFF[order(dirEFF)[5]]) {
+        rolesx = "midlaner"
+      } else if (dirlane[i - 5] == 2) {
+        rolesx = "midlaner"
+      } else if (dirEFF[i - 5] == dirEFF[order(dirEFF)[3]]) {
+        rolesx = "offlaner"
+      } else if (dirgold[i - 5] == dirgold[order(dirgold)[3]]) {
+        rolesx = "offlaner"
+      } else if (dirroam[i - 5]) {
+        rolesx = "roaming_support"
+      } else if (dirSentry[i - 5] == max(dirSentry)) {
+        rolesx = "hard_support"
+      } else if (dirObserver[i - 5] == max(dirObserver)) {
+        rolesx = "hard_support"
+      } else if (dirlane[i - 5] == 1) {
+        rolesx = "offlaner"
+      } else if (dirObserver[i - 5] != 0 && dirObserver[i - 5] < max(dirObserver)) {
+        rolesx = "roaming_support"
+      } else if (dirSentry[i - 5] != 0 && dirSentry[i - 5] < max(dirSentry)) {
+        rolesx = "roaming_support"
+      } else
+        rolesx = "unknown"
+      m_class <- c(m_class, rolesx)
+    }
+    
+  }
+  y = data.frame(lane, eff, HD, GPM, XPM, DN, HH, NK, Observer, Sentry, m_class)
   y[is.na(y)] <- 0
-  write.table(y, file = "datasets.csv",row.names=FALSE, col.names=FALSE, sep=",",append = T,quote = )
+  write.table(
+    y,
+    file = "datasets.csv",
+    row.names = FALSE,
+    col.names = FALSE,
+    sep = ",",
+    append = T,
+    quote =
+  )
 }
 
-# added class for training data - need to clasify more :(
-# full set class
-datasets <- data.frame(read.csv("datasets.csv", sep = ","))
-colnames(datasets)<- c("account_id","level","kda","kill","death", "assist", "observer","sentry","NK", "GPM", "XPM", "LH", "DN","HD", "HH", "TD", "TK", "gold", "XP", "hero_id")
-datasets$hero_id[datasets$hero_id %in% carry] <- "carry"
-datasets$hero_id[datasets$hero_id %in% support] <- "support"
-datasets$hero_id[datasets$hero_id %in% disabler] <- "support"
-datasets$hero_id[datasets$hero_id %in% escape] <- "support"
-datasets$hero_id[datasets$hero_id %in% initiator] <- "ganker"
-datasets$hero_id[datasets$hero_id %in% nuker] <- "ganker"
-datasets$hero_id[datasets$TK > 5 ] <- "pusher"
-datasets$hero_id[datasets$assist > 10 ] <- "roaming-support"
-datasets$hero_id[datasets$HH > 2000 ] <- "support"
-datasets$hero_id[datasets$death > 20 ] <- "feeder"
-datasets$account_id <- NULL
-write.table(datasets, file = "trainingdata.csv",row.names=FALSE, col.names=FALSE, sep=",",append = T,quote = )
+# split dataset into trainig and testing with 70:30
+data = read.csv("datasets_baruloh.csv", sep = "," ,header = F)
 
-# reduced class
-datasets <- data.frame(read.csv("datasets.csv", sep = ","))
-colnames(datasets)<- c("account_id","level","kda","kill","death", "assist", "observer","sentry","NK", "GPM", "XPM", "LH", "DN","HD", "HH", "TD", "TK", "gold", "XP", "hero_id")
-datasets$hero_id[datasets$hero_id %in% carry] <- "carry"
-datasets$hero_id[datasets$hero_id %in% support] <- "support"
-datasets$hero_id[datasets$hero_id %in% disabler] <- "support"
-datasets$hero_id[datasets$hero_id %in% escape] <- "support"
-datasets$hero_id[datasets$hero_id %in% initiator] <- "carry"
-datasets$hero_id[datasets$hero_id %in% nuker] <- "carry"
-datasets$hero_id[datasets$TK > 5 ] <- "carry"
-datasets$hero_id[datasets$assist > 10 ] <- "support"
-datasets$hero_id[datasets$HH > 2000 ] <- "support"
-datasets$account_id <- NULL
-write.table(datasets, file = "reduced_trainingdata.csv",row.names=FALSE, col.names=FALSE, sep=",",append = T,quote = )
+# Sample Indexes
+indexes = sample(1:nrow(data), size=0.3*nrow(data))
+
+# Split data
+test = data[indexes,]
+dim(test)  
+train = data[-indexes,]
+dim(train)
+write.table(
+  test,
+  file = "testing_baruloh.csv",
+  row.names = FALSE,
+  col.names = FALSE,
+  sep = ",",
+  append = T,
+  quote =
+)
+write.table(
+  train,
+  file = "training_baruloh.csv",
+  row.names = FALSE,
+  col.names = FALSE,
+  sep = ",",
+  append = T,
+  quote =
+)
